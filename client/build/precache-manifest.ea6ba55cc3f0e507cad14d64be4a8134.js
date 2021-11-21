811ffac76de0cc745095679ca560c79c67cea392
burhanuddin78 <burhanuddin5023@gmail.com>
2021-11-21 20:54:06 +0500

    change database

---
+++ client/build/precache-manifest.ea6ba55cc3f0e507cad14d64be4a8134.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bda6e3f67c16c6b6c06bb20dc1b43c27",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2436478fb1d45476f1c6",
+    "url": "/static/css/main.daa0e9cc.chunk.css"
+  },
+  {
+    "revision": "dbbec22237cc2880f1bc",
+    "url": "/static/js/2.aa5dc439.chunk.js"
+  },
+  {
+    "revision": "36d397059535b5558d22e250ba3853dd",
+    "url": "/static/js/2.aa5dc439.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2436478fb1d45476f1c6",
+    "url": "/static/js/main.544d2140.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
